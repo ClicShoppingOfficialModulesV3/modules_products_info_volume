4cea3fcffb33e4d4d9dd98582f4069713a05d7ed
ClicShopping - e-commerce Solution <39338579+ClicShopping@users.noreply.github.com>
2018-07-12 09:34:46 -0400

    update json

---
+++ sources/template/Default/modules/modules_products_info/content/products_info_volume.php
@@ -0,0 +1,8 @@
+<div class="<?php echo $text_position; ?> col-md-<?php echo $content_width; ?>">
+  <div class="separator"></div>
+  <div style="float: <?php echo MODULE_PRODUCTS_INFO_VOLUME_POSITION; ?>;">
+    <div class="moduleProductsInfoVolume">
+      <span class="moduleProductsInfoVolume"><h3><?php echo CLICSHOPPING::getDef('text_products_volume') . ' ' . $products_volume; ?></h3></span>
+    </div>
+  </div>
+</div>
